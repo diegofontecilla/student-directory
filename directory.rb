98d7f10963beb5f5e10918974e0e5a34d4d00bc2
diego fontecilla <mail@diegofontecilla.com>
2018-05-06 17:02:42 +0100

    Using while loop on students array

--- directory.rb
+++ directory.rb
@@ -23,10 +23,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    if student[:name].length < 12
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+  index = 0
+  while students[index] != nil
+      puts "#{students[index][:name]} (#{students[index][:cohort]} cohort)"
+      index += 1
   end
 end
 
